01e70cb50fc4cd4a614230b21d8a141f0b4c50d9
Vega <2187815@cognizant.com>
2023-08-29 17:04:15 -0300

    First commit.

---
+++ BlazorSmack/Models/SquareModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorSmack.Models
+{
+    public class SquareModel
+    {
+        private bool isShown { get; set; }
+        public int Id { get; set; }
+        public string Style { get; set; }
+        public bool IsShown
+        {
+            get => isShown;
+            set
+            {
+                isShown = value;
+                if(isShown)
+                {
+                    Style = "mole";
+                }
+                else
+                {
+                    Style = "";
+                }
+            }
+        }
+    }
+}
---
+++ BlazorSmack/Pages/BlazorSmackComponent.cs
@@ -0,0 +1,26 @@
+﻿using BlazorSmack.Models;
+using System.Threading.Tasks.Sources;
+
+namespace BlazorSmack.Pages
+{
+    public partial class BlazorSmackComponent
+    {
+        int score  = 0;
+        int currentTime = 60;
+        int hitPosition = 0;
+        string message = "";
+        int gameSpeed = 500;
+        bool isGameRunning = true;
+
+        public List<SquareModel> Squares { get; set; } = new List<SquareModel>();
+
+        public BlazorSmackComponent()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Squares.Add(new SquareModel { Id = 1 });
+            }
+
+        }
+    }
+}
